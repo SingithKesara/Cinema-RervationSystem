2169810014dafcfd7572a65c5c1a27b531544f6b
SingithKesara <singithkesarawahalathanthri@gmail.com>
2024-12-08 00:04:57 +0530

    Create README.md

---
+++ src/main/java/com/se2groupas/cinema/reservationsystem/CinemaReservationSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.se2groupas.cinema.reservationsystem;
+
+/**
+ *
+ * @author Singith Kesara
+ */
+public class CinemaReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
